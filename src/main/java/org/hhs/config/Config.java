59d305a092eef57bcefb3b13c745e48fa37c1a22
何海水 <270232645@qq.com>
2017-09-03 01:14:32 +0800

    1.0.0

--- src/main/java/org/hhs/config/Config.java
+++ src/main/java/org/hhs/config/Config.java
@@ -3,9 +3,17 @@ package org.hhs.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hewater on 2017/9/2.
@@ -22,5 +30,40 @@ public class Config {
         urlBasedViewResolver.setSuffix(".jsp");
         urlBasedViewResolver.setViewClass(JstlView.class);
         return urlBasedViewResolver;
+    }
+
+    /**
+     * json与对象之间的转换
+     * @return
+     */
+    @Bean
+    public MappingJackson2HttpMessageConverter getMappingJacksonJsonView(){
+        MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
+        return mappingJackson2HttpMessageConverter;
+    }
+
+    /**
+     * json与string之间的转换
+     * @return
+     */
+    @Bean
+    public StringHttpMessageConverter getStringMessageConverter(){
+        StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter();
+        stringHttpMessageConverter.setDefaultCharset(StandardCharsets.UTF_8);
+        return stringHttpMessageConverter;
+    }
+
+    /**
+     * 将上面两个转换器设置到请求映射适配器中
+     * @return
+     */
+    @Bean
+    public RequestMappingHandlerAdapter getRequestMappingHandlerAdapter(){
+        List<HttpMessageConverter<?>> list = new ArrayList<HttpMessageConverter<?>>();
+        list.add(getMappingJacksonJsonView());
+        list.add(getStringMessageConverter());
+        RequestMappingHandlerAdapter requestMappingHandlerAdapter = new RequestMappingHandlerAdapter();
+        requestMappingHandlerAdapter.setMessageConverters(list);
+        return requestMappingHandlerAdapter;
     }
 }
--- src/main/java/org/hhs/controller/HelloController.java
+++ src/main/java/org/hhs/controller/HelloController.java
@@ -1,12 +1,16 @@
 package org.hhs.controller;
 
+import org.hhs.vo.Person;
 import org.springframework.http.server.ServletServerHttpResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hewater on 2017/9/2.
@@ -22,7 +26,19 @@ public class HelloController {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         return "jsp/hello";
+    }
+
+    @RequestMapping("json")
+    @ResponseBody
+    public Object getObj(){
+        List<Person> list = new ArrayList<Person>();
+        for (int i = 0; i < 10; i++){
+            Person person = new Person();
+            person.setAge(String.valueOf(i));
+            person.setName("name"+i);
+            list.add(person);
+        }
+        return list;
     }
 }
